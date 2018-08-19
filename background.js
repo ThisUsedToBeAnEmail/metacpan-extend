e2ce0ee6db0da05a503eea501b7f8fada9dc8b56
LNATION <thisusedtobeanemail@gmail.com>
2018-08-19 22:08:46 +0800

    v1

--- background.js
+++ background.js
@@ -1,9 +1,46 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+function show(hit) {
+	var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+	var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+	var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+	var notification = new Notification(hour + time[2] + ' ' + period, {
+		icon: 'images/meta-48.png',
+		body: hit.main_module + ' v' + hit.version + ' (' + hit.author + ')'
+	});
+	notification.onclick = function () {
+		window.open("https://metacpan.org/pod/" + hit.main_module);
+	};
+}
 
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-	console.log('always on'); // TODO some stuff with browser options
-});
+// Test for notification support.
+if (window.Notification) {
+	setInterval(function() {
+		let recent = localStorage.recent ? JSON.parse(localStorage.recent) : {};
+		_().ajax.post({
+			params: {
+				"query" : {
+					"match": { "status" : "latest" }
+				},
+				sort: { date: 'desc' },
+				size: 20
+			},
+			endpoint: 'release/_search',
+			callback: function (res) {
+				recent.results = res.hits.hits;
+				var shw;
+				if (!recent.notification) recent.notification = {};
+				else shw = 1;
+				res.hits.hits.forEach(function (hi) {
+					hi = hi._source;
+					if (recent.notification[hi.main_module + hi.version] === undefined) {
+						recent.notification[hi.main_module + hi.version] = hi;
+						if (1) {
+							show(hi);
+						}
+					}
+				});
+				localStorage.recent = JSON.stringify(recent);
+			},
+			nospinner: 1,
+		});
+	}, 20000);
+}
--- popup.js
+++ popup.js
@@ -1,213 +1,8 @@
-(function (factory) {
-	factory()
-})(function (undefined) {
-	var Terse = function (selector) {
-		return new TerseCons(selector);
-	},
-	TerseCons = function (selector) {
-		if (selector) this[0] = selector instanceof NodeList ? selector : selector instanceof Element ? [selector] : document.querySelectorAll(selector);
-	},
-	url = 'https://fastapi.metacpan.org/v1/';
-
-	Terse.pro = TerseCons.prototype = {
-		baseUrl: url,
-		style: function (args) {
-			this[0].forEach(function (e) {
-				var key;
-				for (key in args) {
-					e.style[key] = args[key];
-				}
-			});
-		},
-		encodedStr: function (str, obj) {
-			if (typeof str !== 'string') { obj = str; str = '' }
-			for (param in obj) {
-				if (str) str += "&";
-				str += encodeURI(param + "=" + obj[param]);
-			}
-			return str;
-		},
-		equals: function(obj1, obj2) {
-			var keyCount = 0,
-			f = false;
-
-			if (typeof obj1 !== 'object' || typeof obj2 !== 'object') return false;
-			for (var a in obj1) {
-				if (!obj2.hasOwnProperty(a)){ return false }
-
-				if (typeof obj1[a] === 'object' && typeof obj2[a] === 'object'
-					? !this.equals(obj1[a], obj2[a]) : obj1[a] !== obj2[a])
-						{ return false; }
-
-				++keyCount;
-			}
-
-			if (f) return false;
-
-			for (var a in obj2) {
-				if (obj1[a] !== undefined ) --keyCount;
-				else return false;
-			}
-
-			return keyCount !== 0 ? false : true;
-		},
-		element: function () {
-			return this[0][0];
-		},
-		formData: function (selector) {
-			var form = typeof selector === 'string' ? _(selector).element() : selector;
-			var data = {};
-			for (var i = 0; i < form.elements.length; i++) {
-				var field = form.elements[i];
-				field.focus();
-				field.blur();
-				if ( field.name !== undefined && field.name !== '') {
-					var val = (field.type == "radio" || field.type == "checkbox" ? (field.checked ? field.value !== '' ? field.value : true : '') : field.value);
-					if ( val !== undefined && val !== '') data[field.name] = val;
-				}
-			};
-			return data;
-		},
-		factory: function (name, obj) {
-			Object.defineProperty(Terse.pro, name, {
-				get: function() {
-					Object.defineProperty(this, name, {
-						value: new obj(this)
-					});
-					return this[name];
-				},
-				configurable: true,
-				writeable: false,
-			});
-		}
-	};
-
-	window.Terse = window._ = Terse;
-	return Terse;
-});
-
 (function () {
-	var TerseAjax = function (terse) {
-		this.terse = terse;
-	}, lastRequest = {};
-
-	TerseAjax.prototype = {
-		get: function (options) {
-			this.callAjax('GET', options);
-			return this.agent;
-		},
-		post: function (options) {
-			this.callAjax('POST', options);
-			return this.agent;
-		},
-		searchFile: function (options) {
-			options.endpoint = 'file/_search';
-			options.params = {
-				"query" : {
-					"filtered" : {
-						"query" : {
-							"match_phrase" : {
-								"pod.analyzed" : '(' + options.params.q + ')',
-							}
-						},
-						"filter" : {
-							"and" : [
-								{ "term": {"maturity": "released"} },
-								{ "term" : { "status" : "latest" } }
-							]
-						}
-					}
-				}
-			};
-			options.headers = { content_type: 'application/json' }; 
-			this.callAjax('POST', options);
-		},
-		callAjax: function (method, options) {
-			var currentRequestTime = Math.floor(Date.now() / 1000);
-
-			if (lastRequest.params !== undefined && this.terse.equals(lastRequest.params, options.params) === true) {
-				var timeSince = currentRequestTime - lastRequest.time;
-				if (timeSince < 3) return true;
-			}
-
-			lastRequest.params = options.params;
-			lastRequest.time = currentRequestTime;
-
-			var xhttp = new XMLHttpRequest();
-			if (! options.params) options.params = {};
-			if (! options.errorTitle) options.errorTitle = 'unclassified error';
-			
-			// TODO handle browser offline
-
-			var call = this.terse.baseUrl + options.endpoint + '?';
-
-			if (method === 'GET') call += this.terse.encodedStr(options.params);
-
-			var spinner = _('#spinner').element();
-			
-			xhttp.upload.addEventListener('progress', function (evt) {
-				spinner.classList.remove('hide');
-			}, false);
-			
-			// TODO handle errors better :)
-			xhttp.onreadystatechange = function () {
-				if (xhttp.readyState == 4) {
-					console.log(xhttp.response);
-					if (xhttp.status == 200) {
-						spinner.classList.add('hide');
-						var resposne;
-						try {
-							response = JSON.parse(xhttp.response);
-							if (response.error) {
-								console.warn(response.error);
-								return;
-							}
-						} catch (e) {
-							console.warn(e);
-							return;
-						}
-						
-						return options.callback( response, options );
-					} else {
-						console.warn('Ajax failed: ' + xhttp.status);
-					}
-				}
-			}
-
-			xhttp.open(method, call, true);
-			xhttp.timeout = options.timeout || 10000;
-			
-			if (! options.headers ) options.headers = { Access_Control_Allow_Credentials: true, support_credentials: true };
-			if (! options.headers.content_type ) options.headers.content_type = 'application/x-www-form-urlencoded';
-
-			for (header in options.headers) {
-				xhttp.setRequestHeader(
-					header.replace('_', '-'),
-					options.headers[header]
-				);
-			}
-
-			xhttp.withCredentials = true;
-
-			xhttp.ontimeout = function () {
-				options.ontimeout 
-					? options.ontimeout(options)
-					: console.warn('Request Timed Out');
-			}
-
-			xhttp.send( method === 'POST' ? JSON.stringify(options.params) : '' );
-		}
-	};
-	Terse.pro.factory('ajax', TerseAjax);
-})();
-
-// run some logic
-(function () {
-	console.log('yooooo');
 	var imgURL = chrome.extension.getURL("images/metacpan-logo.png");
 	_("#logo").style({backgroundImage:'url(' + imgURL + ')'});
-
-	var render = function (hits) {
+	
+	var renderSearch = function (hits) {
 		html = '';			
 		hits.forEach(function (hit) {
 			console.log(hit);
@@ -220,6 +15,26 @@
 		return html;
 	}
 
+	var renderRecent = function (hits) {
+		html = '';			
+		hits.forEach(function (hit) {
+			console.log(hit);
+			hit = hit._source;
+			html += '<div class="result">';
+			html += '<h2><a href="https://metacpan.org/pod/' + hit.main_module + '">' + hit.main_module + '</a><span class="info">v' + hit.version + ' (' + hit.author + ')</span></h2>';
+			html += '<p>' + (hit.abstract) + '</p>';
+			html += '</div>';
+		});
+		return html;
+	}
+
+	window.addEventListener('click', function (event) {
+		if (event.target.href) {
+			event.preventDefault();
+			chrome.tabs.create({ url: event.target.href });
+		}
+	});
+
 	window.addEventListener('submit', function (event) {
 		event.preventDefault();
 		var params = _().formData(event.target);
@@ -228,35 +43,15 @@
 			callback: function (res) {
 				// i have all this in oo Terse but lets not expose this to the world today :)
 				// instead stringify
-				var results = _('#results').element();
-				var html = render(res.hits.hits);
+				let results = _('#results').element();
+				let html = renderSearch(res.hits !== undefined ? res.hits.hits : res.results.map(function (hit) { console.log(hit); return { _source: hit.hits[0] } }));
 				results.innerHTML = html;
 			}
 		});
 	});
 
-	_().ajax.post({
-		params: {
-			"query" : {
-				"filtered" : {
-					"filter" : {
-						"and" : [
-							{ "term": {"maturity": "released"} },
-							{ "term" : { "status" : "latest" } },
-							{ "exists" : { "field" : "documentation" } }
-						]
-					}
-				}
-			},
-			sort: { date: 'desc' },
-			size: 50
-		},
-		endpoint: 'file/_search',
-		callback: function (res) {
-				var recent = _('#recent').element();
-				var html = render(res.hits.hits);
-				recent.innerHTML = html;
-		},
-		nospinner: 1,
-	});
+	var data = JSON.parse(localStorage.recent);
+	var recent = _('#recent').element();
+	var html = renderRecent(data.results);
+	recent.innerHTML = html;
 })();
